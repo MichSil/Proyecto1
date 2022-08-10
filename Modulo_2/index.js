53b909670b941ffb01d3e62c8e04c5e8dee5bfa4
Michelle Silva <mich.silrod@gmail.com>
2022-08-10 17:59:28 -0500

    Agregando avances

---
+++ Modulo_2/index.js
@@ -0,0 +1,26 @@
+// const edad = 25
+
+
+//     function saludar(){
+
+//         console.log(edad)
+//         let nombre = 'Migue'
+//         console.log(nombre)
+
+//     }
+
+//     function saludar2(){
+        
+//         console.log(edad)
+//         console.log(nombre)
+        
+//     }
+
+// saludar()
+// saludar2()
+
+const nombre = "Michelle"
+const pais = "México"
+const gen = "30"
+
+console.log("bienvenida", nombre, "de", pais, "de la generación", gen)
